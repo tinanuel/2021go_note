c688f6d6ae8c4abcaa48e9553c648d69978ba7bd
tinanuel <majewskakrystyna23@gmail.com>
2021-04-24 10:31:22 +0200

    Small Change

--- src/App.vue
+++ src/App.vue
@@ -99,7 +99,7 @@
   }
 
   .cover {
-    background-image: url("./assets/background.jpg");
+    background-image: url("./assets/background2.jpg");
     background-position: center;
     background-size: cover;
     height: 30%;
